63be29f2f407cc8ba9355a1b0100e302619b103e
Francis Ricardo Ramirez <francisricardor@gmail.com>
2021-01-21 23:30:37 -0400

    Implementando LocalStorage.

--- FrontEnd/src/app/user/register/register.component.ts
+++ FrontEnd/src/app/user/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from '../Model/User';
+import { UserService } from '../services/user.service';
+import * as alertify from 'alertifyjs';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -8,25 +10,79 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  registerationForm: FormGroup
-  constructor() { }
+  registerationForm: FormGroup;
+  user:User;
+  userSubmitted:boolean;
+  constructor(private fb:FormBuilder, private userService:UserService) {
+
+   }
 
   ngOnInit() {
-    this.registerationForm = new FormGroup({
-      userName: new FormControl('Mark',Validators.required),
-      email:new FormControl(null,[Validators.required,Validators.email]),
-      password:new FormControl(null,[Validators.required, Validators.minLength(8)]),
-      confirmPassword:new FormControl(null,[Validators.required]),
-      mobile:new FormControl(null,[Validators.required, Validators.maxLength(10)])
-    },this.passwordMatchingValidator);
+    this.createRegisterationForm();
+    localStorage.clear();
   }
   onSubmit(){
-    console.log(this.registerationForm);
+
+    this.userSubmitted= true;
+
+    if (this.registerationForm.valid) {
+
+      this.user = this.userInformation;
+      this.userService.addUser(this.user);
+      this.registerationForm.reset();
+      this.userSubmitted=false;
+      alertify.success('Congrats, you are successfully registered.');
+
+    }
+    else{
+      alertify.error('Kindly provide the required fields');
+    }
+
+  }
+  createRegisterationForm(){
+    this.registerationForm =  this.fb.group({
+      userName:[null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      password:[null, [Validators.required, Validators.minLength(8)]],
+      confirmPassword:[null, Validators.required],
+      mobile:[null,[Validators.required, Validators.maxLength(10)]]
+   },{validators:this.passwordMatchingValidator});
   }
   passwordMatchingValidator(fg:FormGroup):Validators{
     return fg.get('password').value === fg.get('confirmPassword').value ? null :
     {
       notmatched:true
-    }
+    };
+  }
+
+  get userName()
+  {
+    return this.registerationForm.get('userName') as FormControl;
+  }
+  get email()
+  {
+    return this.registerationForm.get('email') as FormControl;
+  }
+  get password()
+  {
+    return this.registerationForm.get('password') as FormControl;
+  }
+  get confirmPassword()
+  {
+    return this.registerationForm.get('confirmPassword') as FormControl;
+  }
+  get mobile()
+  {
+    return this.registerationForm.get('mobile') as FormControl;
+  }
+  get userInformation(): User{
+
+    return this.user = {
+        userName: this.userName.value,
+        email:this.email.value,
+        password:this.password.value,
+        confirmPassword:this.confirmPassword.value,
+        mobile:this.mobile.value
+    };
   }
 }
